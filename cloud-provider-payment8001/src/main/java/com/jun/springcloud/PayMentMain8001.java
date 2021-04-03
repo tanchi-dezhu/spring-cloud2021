749ce0b2c89bcc60234fbeaad011faf92bc8b7a9
jun <884720982@qq.com>
2021-04-03 18:19:43 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/jun/springcloud/PayMentMain8001.java
@@ -0,0 +1,11 @@
+package com.jun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PayMentMain8001.class,args);
+    }
+}

9c9ea129fb3d683883cf984422f8fa61d1d9c257
jun <884720982@qq.com>
2021-04-11 14:30:13 +0800

    hystrix消费端服务降级代码提交

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/jun/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/jun/springcloud/OrderHystrixMain80.java
@@ -2,6 +2,7 @@ package com.jun.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class OrderHystrixMain80
 {
     public static void main(String[] args)
--- cloud-consumer-feign-hystrix-order80/src/main/java/com/jun/springcloud/controller/OrderHystirxController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/jun/springcloud/controller/OrderHystirxController.java
@@ -1,6 +1,8 @@
 package com.jun.springcloud.controller;
 
 import com.jun.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,9 +29,16 @@ public class OrderHystirxController
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id)
     {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
+    }
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id)
+    {
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
     }
 }

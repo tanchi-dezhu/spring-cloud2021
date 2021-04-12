3f2052e1e0c97fcc74f78f4c32a14feb657f050d
jun <884720982@qq.com>
2021-04-12 10:05:46 +0800

    提交hystrix熔断处理机制

--- cloud-provider-hystrix-payment8001/src/main/java/com/jun/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/jun/springcloud/controller/PaymentController.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
-public class PaymentController
-{
+public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
@@ -24,18 +23,23 @@ public class PaymentController
 
 
     @GetMapping("/payment/hystrix/ok/{id}")
-    public String paymentInfo_OK(@PathVariable("id") Integer id)
-    {
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
         String result = paymentService.paymentInfo_OK(id);
-        log.info("****result: "+result);
+        log.info("****result: " + result);
         return result;
     }
 
     @GetMapping("/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) throws InterruptedException
-    {
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) throws InterruptedException {
         String result = paymentService.paymentInfo_TimeOut(id);
-        log.info("****result: "+result);
+        log.info("****result: " + result);
+        return result;
+    }
+
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("****result: " + result);
         return result;
     }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/jun/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/jun/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.jun.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,34 +13,61 @@ import java.util.concurrent.TimeUnit;
  * @date 2021年04月10日 20:41
  */
 @Service
-public class PaymentService
-{
+public class PaymentService {
     /**
      * 正常访问，一切OK
+     *
      * @param id
      * @return
      */
-    public String paymentInfo_OK(Integer id)
-    {
-        return "线程池:"+Thread.currentThread().getName()+"paymentInfo_OK,id: "+id+"\t"+"O(∩_∩)O";
+    public String paymentInfo_OK(Integer id) {
+        return "线程池:" + Thread.currentThread().getName() + "paymentInfo_OK,id: " + id + "\t" + "O(∩_∩)O";
     }
 
     /**
      * 超时访问，演示降级
+     *
      * @param id
      * @return
      */
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
     })
-    public String paymentInfo_TimeOut(Integer id)
-    {
+    public String paymentInfo_TimeOut(Integer id) {
         int second = 5;
-        try { TimeUnit.SECONDS.sleep(second); } catch (InterruptedException e) { e.printStackTrace(); }
-        return "线程池:"+Thread.currentThread().getName()+"paymentInfo_TimeOut,id: "+id+"\t"+"O(∩_∩)O，耗费秒: "+second;
+        try {
+            TimeUnit.SECONDS.sleep(second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池:" + Thread.currentThread().getName() + "paymentInfo_TimeOut,id: " + id + "\t" + "O(∩_∩)O，耗费秒: " + second;
     }
-    public String paymentInfo_TimeOutHandler(Integer id){
-        return "/(ㄒoㄒ)/调用支付接口超时或异常：\t"+ "\t当前线程池名字" + Thread.currentThread().getName();
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "/(ㄒoㄒ)/调用支付接口超时或异常：\t" + "\t当前线程池名字" + Thread.currentThread().getName();
+    }
+
+
+    //=========服务熔断
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),//是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),//请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),//时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),//失败率多少后跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("******id 不能负数");
+        }
+
+        //等价于UUID.randomUUID().toString()
+        String serialNumber = IdUtil.simpleUUID();
+
+        return Thread.currentThread().getName() + "\t" + "调用成功，流水号: " + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "id 不能负数，请稍后再试，/(ㄒoㄒ)/~~   id: " + id;
     }
 }
 

275f05b859dfabfa95bec7a3e6653a5641bfdc2f
jun <884720982@qq.com>
2021-04-13 20:39:11 +0800

    提交hystrix监控代码

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/jun/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.jun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author jun
+ * @date 2021年04月13日 20:23
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}

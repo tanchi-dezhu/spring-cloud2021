6adf3de686b9dd9b8897c196f28d69965a6d4735
jun <884720982@qq.com>
2021-04-16 21:51:20 +0800

    添加新的gateway路由匹配规则

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author jun
+ * @date 2021年04月16日 21:35
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}

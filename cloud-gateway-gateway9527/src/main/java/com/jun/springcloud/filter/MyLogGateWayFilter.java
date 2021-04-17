9f617a8194da4895296ac9d6ae0381235e702b14
jun <884720982@qq.com>
2021-04-17 20:50:20 +0800

    自定义gateway Filter过滤

---
+++ cloud-gateway-gateway9527/src/main/java/com/jun/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,39 @@
+package com.jun.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author jun
+ * @date 2021年04月17日 20:33
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("进入Filter" + new Date());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (StringUtils.isBlank(uname)){
+            log.info("用户名不存在");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}

70fd3063ef2175649567dd016b9ea6f3275dbbbb
jun <884720982@qq.com>
2021-04-21 09:50:14 +0800

    客户端获取配置中心中github的配置数据http://localhost:3355/configInfo

---
+++ cloud-config-client-3355/src/main/java/com/jun/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.jun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jun
+ * @date 2021年04月21日 9:38
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}

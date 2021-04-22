860d552a01fc22b80b1af272741d735bd194b39e
jun <884720982@qq.com>
2021-04-22 21:37:07 +0800

    config结合bus实现一处修改处处生效，采用消息总共只需要发送一次post请求curl -X POST "http://localhost:3355/actuator/refresh"，3355、3366配置也会随之更新

---
+++ cloud-config-client-3366/src/main/java/com/jun/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.jun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jun
+ * @date 2021年04月22日 20:53
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}

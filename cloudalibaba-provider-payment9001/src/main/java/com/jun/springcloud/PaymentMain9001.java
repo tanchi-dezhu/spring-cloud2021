9564ec40cfc6129ff68aaf9a34af0f9c17d3951a
jun <884720982@qq.com>
2021-04-28 10:12:48 +0800

    使用nacos实现服务注册

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/jun/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.jun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author jun
+ * @date 2021年04月28日 9:56
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/jun/springcloud/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.jun.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jun
+ * @date 2021年04月28日 9:56
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}

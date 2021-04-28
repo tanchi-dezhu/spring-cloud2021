18794db40fe54a5eb6a2cc1d8738067cc2a399f1
jun <884720982@qq.com>
2021-04-28 10:31:05 +0800

    使用nacos添加9002集群

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/jun/springcloud/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package com.jun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author jun
+ * @date 2021年04月28日 10:14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
